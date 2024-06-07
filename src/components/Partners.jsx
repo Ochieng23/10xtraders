91c73ecbda0ef5cb7ad6bc46294bc824bac9c123
paul <ochiengpaul001@gmail.com>
2024-06-07 20:23:10 +0300

    correct margins in hero section

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -11,7 +11,7 @@ import blueberry from "../images/Official-Partners-Logo-ACY.svg";
 import eightcup from "../images/Eightcap-Website-Logo.svg";
 import avatrade from "../images/avatrade1-300x43.webp";
 import acy from "../images/Official-Partners-Logo-300x46.webp";
-
+import fxtm from "../images/logo-dark.svg";
 const logos = [
   { name: "Interactive Brokers", logo: interactive },
   { name: "Fidelity", logo: fidelity },
@@ -24,17 +24,18 @@ const logos = [
   { name: "Eightcap", logo: eightcup },
   { name: "AvaTrade", logo: avatrade },
   { name: "ACY Securities", logo: acy },
+  { name: "FXTM", logo: fxtm },
 ];
 
 function Partners() {
   return (
-    <div className="bg-white py-8 px-4 sm:px-6 lg:px-8">
+    <div className="bg-white py-8 px-10 sm:px-6 lg:px-8">
       <p className="font-bold text-xl text-gray-600 text-center mb-6">
         Free via our partner brokers & exchanges
       </p>
       <ul
         role="list"
-        className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 justify-center items-center"
+        className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 justify-center items-center"
       >
         {logos.map((company) => (
           <li key={company.name} className="flex justify-center">
