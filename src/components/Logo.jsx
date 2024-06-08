95fe0dbd91617aed8de89c2f3af98e61c5caa25e
paul <ochiengpaul001@gmail.com>
2024-06-08 13:53:02 +0300

    correct margins in hero section

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import logo from "../images/1.svg"
+import logo from "../images/2.svg"
 
 export function Logo(props) {
   return (
-    <Image src={logo} alt="Logo" width={230} height={230} />
+    <Image src={logo} alt="Logo" width={350} height={350} />
   )
 }
