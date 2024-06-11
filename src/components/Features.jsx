a5d2e4bc737de34c8150548a73654c35f5d9a3eb
paul <ochiengpaul001@gmail.com>
2024-06-11 16:53:17 +0300

    pre-build

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -13,7 +13,7 @@ const posts = [
     mainImage: img1,
     title: "Optimize & Plan",
     description:
-      "Effortlessly enhance and fine-tune your strategies with our cutting-edge testing and simulation tools. Our comprehensive insights and analysis resources empower you to make informed trading decisions, boosting your performance and maximizing your returns.s",
+      "Effortlessly enhance and fine-tune your strategies with our cutting-edge testing and simulation tools. Our comprehensive insights and analysis resources empower you to make informed trading decisions, boosting your performance and maximizing your returns.",
   },
   {
     _id: "2",
@@ -31,7 +31,7 @@ const posts = [
     mainImage: img1,
     title: "Automated Signals",
     description:
-      "Create personalized alerts to stay informed wherever you are. 10xTraders.ai scripts continuously monitors real-time market trends, essential financial indicators, and economic developments 24/7, ensuring you never miss a crucial update.",
+      "Create personalized alerts to stay informed wherever you are. 10xTraders.ai scripts continuously monitor real-time market trends, essential financial indicators, and economic developments 24/7, ensuring you never miss a crucial update.",
   },
   {
     _id: "4",
@@ -56,21 +56,31 @@ function Features() {
   }
 
   return (
-    <div className="mt-0 flex-col gap-10 bg-white px-10 py-10">
+    <div className="mt-0 flex flex-col gap-10 bg-white px-10 py-10">
       {posts.map((post, index) => (
-        <Link href="#" key={post._id}>
+        <Link href="#" key={post._id} className="no-underline hover:no-underline">
           <div
-            className={`mt-2 flex flex-col gap-7 rounded-md md:flex-row ${
+            className={`mt-2 flex flex-col md:flex-row items-center ${
               index % 2 === 0 ? "" : "md:flex-row-reverse"
             }`}
           >
+            {/* Image Section */}
+            <div className="md:w-1/2 p-4">
+              <Image
+                src={post.mainImage}
+                width={400}
+                height={400}
+                className="object-cover"
+                alt="main blog image"
+              />
+            </div>
             {/* Text Section */}
-            <div className="flex w-full flex-col justify-between px-8 py-20 md:w-3/5">
+            <div className="flex w-full flex-col justify-between p-4 md:w-1/2">
               <div className="flex flex-col gap-4">
                 <h3 className="text-yellow-400 font-semibold">{post.tag}</h3>
                 <h2 className="text-4xl font-bold text-black">{post.title}</h2>
                 <p className="text-black text-xl">
-                  {truncateText(post.description, 70)}
+                  {truncateText(post.description, 30)}
                 </p>
                 <Link
                   href="#"
@@ -80,16 +90,6 @@ function Features() {
                   <GoArrowUpRight className="ml-2" />
                 </Link>
               </div>
-            </div>
-            {/* Image Section */}
-            <div className="group relative overflow-hidden rounded-md md:w-2/5">
-              <Image
-                src={post.mainImage}
-                width={400}
-                height={400}
-                className="max-h-[400px] w-full object-cover"
-                alt="main blog image"
-              />
             </div>
           </div>
         </Link>
